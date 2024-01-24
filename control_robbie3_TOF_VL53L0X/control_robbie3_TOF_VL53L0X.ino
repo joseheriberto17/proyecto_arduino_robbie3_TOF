baac0d062aebe75466bdf9c965b29bc3af32e1c8
theheri24 <jose.heriberto@hotmail.es>
2024-01-24 00:29:25 -0500

    el programa puede leer los 6 sensores

--- control_robbie3_TOF_VL53L0X/control_robbie3_TOF_VL53L0X.ino
+++ control_robbie3_TOF_VL53L0X/control_robbie3_TOF_VL53L0X.ino
@@ -19,6 +19,10 @@
 
 VL53L0X sensor1;
 VL53L0X sensor2;
+VL53L0X sensor3;
+VL53L0X sensor4;
+VL53L0X sensor5;
+VL53L0X sensor6;
 
 
 // Uncomment this line to use long range mode. This
@@ -66,16 +70,28 @@ void setup()
   Serial.begin(115200);
   Wire.begin();
 
-  // inicio de los pines de Habilitadores, Total: 2  
+  // inicio de los pines de Habilitadores, Total: 6 sensores 
   pinMode(8,OUTPUT);
   pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(3,OUTPUT);
 
   digitalWrite(8,LOW);
   digitalWrite(7,LOW);
+  digitalWrite(6,LOW);
+  digitalWrite(5,LOW);
+  digitalWrite(4,LOW);
+  digitalWrite(3,LOW);
 
-  // inicio del sensor 1
+  // inicio de los sensor TOF
   inicio_sensor_TOF(sensor1,8,0x01);
   inicio_sensor_TOF(sensor2,7,0x02);
+  inicio_sensor_TOF(sensor3,6,0x03);
+  inicio_sensor_TOF(sensor4,5,0x04);
+  inicio_sensor_TOF(sensor5,4,0x05);
+  inicio_sensor_TOF(sensor6,3,0x06);
   
   
 
@@ -91,19 +107,36 @@ void setup()
   // reduce timing budget to 20 ms (default is about 33 ms)
   sensor1.setMeasurementTimingBudget(20000);
   sensor2.setMeasurementTimingBudget(20000);
+  sensor3.setMeasurementTimingBudget(20000);
+  sensor4.setMeasurementTimingBudget(20000);
+  sensor5.setMeasurementTimingBudget(20000);
+  sensor6.setMeasurementTimingBudget(20000);
 #elif defined HIGH_ACCURACY
   // increase timing budget to 200 ms
   sensor1.setMeasurementTimingBudget(200000);
   sensor2.setMeasurementTimingBudget(200000);
+  sensor3.setMeasurementTimingBudget(200000);
+  sensor4.setMeasurementTimingBudget(200000);
+  sensor5.setMeasurementTimingBudget(200000);
+  sensor6.setMeasurementTimingBudget(200000);
 #endif
 }
 
 void loop()
 {
+  // medicion de todos los sensores.
   medir_distancia_TOF(sensor1);
   Serial.print(" ");
   medir_distancia_TOF(sensor2);
-
+  Serial.print(" ");
+  medir_distancia_TOF(sensor3);
+  Serial.print(" ");
+  medir_distancia_TOF(sensor4);
+  Serial.print(" ");
+  medir_distancia_TOF(sensor5);
+  Serial.print(" ");
+  medir_distancia_TOF(sensor6);
+  
   Serial.println();
 }
 
